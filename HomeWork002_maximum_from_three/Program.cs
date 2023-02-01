47cecbc34739741187490f70153360e6fb2b0377
Филипп <111292884+FANTICK33@users.noreply.github.com>
2023-02-01 12:08:34 +0700

    HomeWork002

---
+++ HomeWork002_maximum_from_three/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Введите три числа по очереди:");
+int number_one = Convert.ToInt32(Console.ReadLine());
+int number_two = Convert.ToInt32(Console.ReadLine());
+int number_three = Convert.ToInt32(Console.ReadLine());
+
+if(number_one > number_two)
+{
+    if(number_one > number_three)
+    {
+        Console.WriteLine($"Первое число: {number_one} - большее");
+    }
+    else
+    {
+        Console.WriteLine($"Третье число: {number_three} - большее");
+    }
+}
+else
+{
+    if(number_two > number_three)
+    {
+        Console.WriteLine($"Второе число: {number_two} - большее");
+    }
+    else
+    {
+        Console.WriteLine($"Третье число: {number_three} - большее");
+    }
+}
