427d60ffc0d1c4f149617657c2211c5e8bb5f753
Филипп <111292884+FANTICK33@users.noreply.github.com>
2023-02-01 12:33:17 +0700

    HomeWork004

---
+++ HomeWork004_even_numbers_from_1_to_N/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число для нахождения четных чисел от 1 до ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int count = 0;
+while (count < number)
+{
+    count++;
+    if(count%2 == 0)
+    {
+        Console.WriteLine(count);
+    }
+}
