922da8deea517ca3776125b0c4e2d588c073146f
Филипп <111292884+FANTICK33@users.noreply.github.com>
2023-02-03 22:57:05 +0700

    HomeWork007

---
+++ Урок 2. Базовые алгоритмы/HomeWork007_is_weekends/Program.cs
@@ -0,0 +1,15 @@
+﻿string Weekends(int Day)
+{
+    string result = "Нет";
+    if (Day == 6 || Day == 7)
+    {
+        result = "Да";
+    }
+    return result;
+}
+
+Console.Write("Введите день недели: ");
+int Day = Convert.ToInt32(Console.ReadLine());
+
+string IsDate = Weekends(Day);
+Console.WriteLine(IsDate);
