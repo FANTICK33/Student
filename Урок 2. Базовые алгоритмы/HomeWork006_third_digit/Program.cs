0e5408187fa4295bf3b79e26bc033a6ffd595bd4
Филипп <111292884+FANTICK33@users.noreply.github.com>
2023-02-03 21:31:47 +0700

    HomeWork006

---
+++ Урок 2. Базовые алгоритмы/HomeWork006_third_digit/Program.cs
@@ -0,0 +1,20 @@
+﻿int IsThird(int number)
+{
+    int result = -1;
+    if (number >= 100)
+    {
+        while (number > 999)
+            {
+                number = number / 10;
+            }
+        result = number % 10;
+    }
+    return result;
+
+}
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int Third = IsThird(number);
+Console.WriteLine($"{Third} - третья цифра, из числа - {number}");
