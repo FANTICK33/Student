5b584d9bd3da98bdd715ca3811cc31880d38ced9
Филипп <111292884+FANTICK33@users.noreply.github.com>
2023-02-03 17:14:58 +0700

    HomeWork005

---
+++ Урок 2. Базовые алгоритмы/HomeWork005_second_digit/Program.cs
@@ -0,0 +1,12 @@
+﻿int IsSecond(int number)
+{
+    int SecondNumber = (number % 100) / 10;
+    return SecondNumber;
+}
+
+Console.Write("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int Second = IsSecond(number);
+
+Console.WriteLine($"{Second} - второе цифра, из числа {number}");
