bf5030380d6c67ac5c0f9d4e18edd18b0aa27c93
Филипп <111292884+FANTICK33@users.noreply.github.com>
2023-02-09 22:43:11 +0700

    Updated HomeWork008

--- Урок 3. Базовые алгоритмы. Продолжение/HomeWork008_palindrome/Program.cs
+++ Урок 3. Базовые алгоритмы. Продолжение/HomeWork008_palindrome/Program.cs
@@ -1,10 +1,10 @@
-﻿System.Console.Write("Ввидите пятизначное число: ");
-int num = Convert.ToInt32(Console.ReadLine());
-int TenThousand = num/10000;
-int Ten = num%10;
-int Thousand = (num - (TenThousand * 10000))/1000;
-int Hundred = ((num-Ten)%100)/10;
-if((TenThousand==Ten)&(Thousand==Hundred))
-System.Console.WriteLine($"число: {num} - является полиндром");
-else
-System.Console.WriteLine($"число: {num} - НЕ является полиндромом");
+﻿bool Palindrom(string num)
+{
+    for (int i = 0; i < num.Length / 2; i++)
+        if (num[i] != num[num.Length - i - 1])
+            return false;
+    return true;
+}
+Console.Write("Ввидите число: ");
+string Number = Console.ReadLine();
+System.Console.WriteLine(Palindrom(Number));
