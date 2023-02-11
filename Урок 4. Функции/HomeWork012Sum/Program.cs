35485db4a612477546788177b3c4c872467d0f1c
Филипп <111292884+FANTICK33@users.noreply.github.com>
2023-02-11 14:02:00 +0700

    HomeWork012

---
+++ Урок 4. Функции/HomeWork012Sum/Program.cs
@@ -0,0 +1,15 @@
+﻿void Sum(int num)
+{
+    int result = 0;
+    int sum = 0;
+    while (num != 0)
+        {
+            result = num % 10;
+            num = num / 10;
+            sum = sum + result;
+        }
+    Console.WriteLine(sum);
+}
+Console.WriteLine("Введите число: ");
+int MyNum = Convert.ToInt32(Console.ReadLine());
+Sum(MyNum);
