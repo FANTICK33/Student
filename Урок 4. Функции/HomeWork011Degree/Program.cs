6a646fc5a7015661c109f85ccd20e6d70c38bd48
Филипп <111292884+FANTICK33@users.noreply.github.com>
2023-02-11 10:58:29 +0700

    HomeWork011

---
+++ Урок 4. Функции/HomeWork011Degree/Program.cs
@@ -0,0 +1,14 @@
+﻿void Degree(int number, int degree)
+{
+    int result = 1;
+    for (int i = 1; i <= degree; i++)
+    {
+        result = result * number;
+    }
+    Console.WriteLine($"{result} возведенное число {number} в натуральную степень {degree}");
+}
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите степень: ");
+int deg = Convert.ToInt32(Console.ReadLine());
+Degree(num,deg);
