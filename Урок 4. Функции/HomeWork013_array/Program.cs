567384821403a1f61dca89af5828f66791d66abd
Филипп <111292884+FANTICK33@users.noreply.github.com>
2023-02-12 03:51:52 +0700

    HomeWork013

---
+++ Урок 4. Функции/HomeWork013_array/Program.cs
@@ -0,0 +1,23 @@
+﻿int[] Array(int size)
+{
+    int[] array = new int [size];
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"Введите число для индекс - {i}: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+void PrintArray(int[] array)
+{
+    Console.Write("Полученный массив: [");
+    for (int i = 0; i < array.Length-1; i++)
+        Console.Write(array[i] + ", ");
+    Console.Write(array[array.Length-1]);
+    Console.WriteLine("]");
+}
+
+System.Console.Write("Введите длину нового массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] MyArray = Array(size);
+PrintArray(MyArray);
