39f140c27f75d4dc7c9959a7d6c712f44ec17206
Филипп <111292884+FANTICK33@users.noreply.github.com>
2023-02-13 22:11:25 +0700

    HomeWork015

---
+++ Урок 5. Функции и одномерные массивы/HomeWork015SumOfOddPositions/Program.cs
@@ -0,0 +1,42 @@
+﻿int[] CreateRandomArray(int size)
+{
+    int[] array = new int [size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(0,10);
+    }
+    return array;
+}
+
+int Sum(int[] MyArray)
+{
+    int sum = 0;
+    for (int i = 0; i < MyArray.Length; i++)
+    {
+        if(i%2!=0)
+        sum += MyArray[i];
+    }
+    return sum;
+}
+
+void PrintArray(int[] MyArray)
+{
+    Console.Write("Created array: ");
+    for (int i = 0; i < MyArray.Length; i++)
+    {
+        Console.Write(i + "-" + MyArray[i] + ", ");
+    }
+}
+
+void PrintSum(int Sum)
+{
+    Console.WriteLine("\nThe sum under odd indexes: " + Sum);
+}
+
+Console.Write("Input the length of new array: ");
+int Size = Convert.ToInt32(Console.ReadLine());
+int[] MyArray = CreateRandomArray(Size);
+PrintArray(MyArray);
+
+int SumUnderOddIndexes = Sum(MyArray);
+PrintSum(SumUnderOddIndexes);
