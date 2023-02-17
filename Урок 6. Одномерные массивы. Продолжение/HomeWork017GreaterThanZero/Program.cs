2494f125324be79085847b5f10ee365c4a48bfe3
Филипп <111292884+FANTICK33@users.noreply.github.com>
2023-02-18 02:13:02 +0700

    HomeWork017
    HomeWork017GreaterThanZero

---
+++ Урок 6. Одномерные массивы. Продолжение/HomeWork017GreaterThanZero/Program.cs
@@ -0,0 +1,13 @@
+﻿int InputNambers(int check)
+{
+    int count = 0;
+    for (int i = 0; i < check; i++)
+    {
+        Console.Write("Enter the number "+(i+1)+": ");
+        if (Convert.ToInt32(Console.ReadLine())>0)
+            count += 1;
+    }
+    return count;
+}
+Console.Write("How many digits to check?: ");
+Console.WriteLine(InputNambers(Convert.ToInt32(Console.ReadLine()))+" - more than zero");
