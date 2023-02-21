feb8aee79c6f22819ef9ca50aaf9eb84af188525
Филипп <111292884+FANTICK33@users.noreply.github.com>
2023-02-21 23:12:02 +0700

    Update HomeWork019

--- Урок 7. Двумерные массивы/HomeWork019CreatingTwo-dimensionalArray/Program.cs
+++ Урок 7. Двумерные массивы/HomeWork019CreatingTwo-dimensionalArray/Program.cs
@@ -1,19 +1,19 @@
-﻿int[,] CreateTwoDimensionalArray(int rows, int columns) //Метод создания двумерного массива.
+﻿double[,] CreateTwoDimensionalArray(int rows, int columns) //Метод создания двумерного массива.
 {
-    int[,] Array = new int [rows, columns];
+    double[,] Array = new double [rows, columns];
     for (int i = 0; i < rows; i++)
         for (int j = 0; j < columns; j++)
-            Array[i,j] = new Random().Next(0,100);
+            Array[i,j] = (new Random().NextDouble()* 2 - 1) * 10; //Заполнение случайными вещественными числами.
     return Array;
 }
-void ShowArray(int[,] Array) //Вывод двумерного массива в консоль.
+void ShowArray(double[,] Array) //Вывод двумерного массива в консоль.
 {
     int rows = Array.GetUpperBound(0) + 1;
     int columns = Array.GetUpperBound(1) + 1;
     for (int i = 0; i < rows; i++)
     {
         for (int j = 0; j < columns; j++)
-            Console.Write($"{Array[i, j]} \t");
+            Console.Write($"{Math.Round(Array[i, j],1)} \t");
         Console.WriteLine();
     }
 }
@@ -22,5 +22,5 @@ int Rows = Convert.ToInt32(Console.ReadLine());
 Console.Write("Input the number of the columns two-dimensional array: "); //Задаем кол-во столбцов.
 int Columns = Convert.ToInt32(Console.ReadLine());
 // Вызов методов.
-int[,] TwoDimensionalArray = CreateTwoDimensionalArray(Rows,Columns);
+double[,] TwoDimensionalArray = CreateTwoDimensionalArray(Rows,Columns);
 ShowArray(TwoDimensionalArray);
