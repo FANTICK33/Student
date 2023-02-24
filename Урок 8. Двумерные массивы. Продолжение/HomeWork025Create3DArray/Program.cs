4c3e11dd699c0994dc5468ea0c9fa5f3b618e25d
Филипп <111292884+FANTICK33@users.noreply.github.com>
2023-02-25 01:39:49 +0700

    HomeWork025

---
+++ Урок 8. Двумерные массивы. Продолжение/HomeWork025Create3DArray/Program.cs
@@ -0,0 +1,19 @@
+﻿int[,,] Create3DArray(){
+    int[,,] Array = new int[2,2,2];
+    for (int i = 0; i < Array.GetLength(0); i++)
+        for (int j = 0; j < Array.GetLength(1); j++)
+            for (int k = 0; k < Array.GetLength(2); k++)
+                Array[i,j,k] = new Random().Next(10,100);
+    return Array;
+}
+void Print3DArray(int[,,] Array){
+    for (int i = 0; i < Array.GetLength(0); i++)
+        for (int j = 0; j < Array.GetLength(1); j++)
+        {
+            for (int k = 0; k < Array.GetLength(2); k++)
+                Console.Write($"{Array[i,j,k]}" + (i,j,k) + " ");
+            Console.WriteLine();
+        }
+}
+
+Print3DArray(Create3DArray());
