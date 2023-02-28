6799559ec942aa8ec846d46cdf990d725cc99810
Филипп <111292884+FANTICK33@users.noreply.github.com>
2023-02-28 16:40:29 +0700

    AckermannFunction

---
+++ Урок 9. Рекурсия/HomeWork029AckermannFunction/Program.cs
@@ -0,0 +1,15 @@
+﻿int AckermannFunction(int M, int N)
+{
+    if (M == 0)
+        return N + 1;
+    else if (M != 0 && N == 0)
+        return AckermannFunction(M - 1, 1);
+    else
+        return AckermannFunction(M - 1, AckermannFunction(M, N - 1));
+}
+Console.Write("Input number M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input number N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int result = AckermannFunction(M, N);
+Console.WriteLine(result);
