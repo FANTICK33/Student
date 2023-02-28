28d1d426dc62aebfda926d43e546e2ebc51239a8
Филипп <111292884+FANTICK33@users.noreply.github.com>
2023-02-28 16:14:18 +0700

    NaturalGap

---
+++ Урок 9. Рекурсия/HomeWork028NaturalGap/Program.cs
@@ -0,0 +1,13 @@
+﻿void NaturalInterval(int M, int N)
+{
+    Console.Write(M + " ");
+    if (M > N)
+        NaturalInterval(M - 1, N);
+    if (M < N)
+        NaturalInterval(M + 1, N);
+}
+Console.Write("Input number M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input number N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+NaturalInterval(M, N);
