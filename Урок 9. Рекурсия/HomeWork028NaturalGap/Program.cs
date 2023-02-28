c46f46a7b34037979dc186d41a0d1bd8b553c108
Филипп <111292884+FANTICK33@users.noreply.github.com>
2023-02-28 12:16:13 +0700

    HomeWork028NaturalGap

---
+++ Урок 9. Рекурсия/HomeWork028NaturalGap/Program.cs
@@ -0,0 +1,18 @@
+﻿int RecursionOfNaturalNumbers(int M, int N)
+{
+    int Sum = 0;
+    if (M < N)
+    {
+        for (int i = M; i <= N; i++)
+        {
+            Sum += i;
+        }
+    }
+    return Sum;
+}
+Console.Write("Input number M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input number N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int sum = RecursionOfNaturalNumbers(M, N);
+Console.WriteLine(sum);
