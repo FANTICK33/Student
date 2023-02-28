4fe6bf8c9f96db233b3db5765e37121736874959
Филипп <111292884+FANTICK33@users.noreply.github.com>
2023-02-28 14:07:03 +0700

    NaturalRecursion

---
+++ Урок 9. Рекурсия/HomeWork027NaturalRecursion/Program.cs
@@ -0,0 +1,9 @@
+﻿void RecursionOfNaturalNumbers(int Num)
+{
+    Console.Write(Num + " ");
+    if (Num > 1)
+        RecursionOfNaturalNumbers(Num - 1);
+}
+Console.Write("Input number: ");
+int Num = Convert.ToInt32(Console.ReadLine());
+RecursionOfNaturalNumbers(Num);
